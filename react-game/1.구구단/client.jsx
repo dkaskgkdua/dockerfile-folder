a842b27dbdcc5daa19ece4819cf07494866e18b6
SongMinJun <dkaskgkdua@gmail.com>
2022-01-02 22:05:22 +0900

    1. 웹팩 및 바벨 설정

--- react-game/1.구구단/client.jsx
+++ react-game/1.구구단/client.jsx
@@ -1,6 +1,6 @@
 const React = require('react')
 const ReactDom = require('react-dom')
 // 클래스
-const WordRelay = require('./WordRelay')
+const WordRelay = require('./WordRelayHooks')
 
 ReactDom.render(<WordRelay/>, document.querySelector('#root'))
--- react-game/1.구구단/webpack.config.js
+++ react-game/1.구구단/webpack.config.js
@@ -1,4 +1,7 @@
 const path = require('path');
+const RefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin')
+const webpack = require('webpack');
+
 
 module.exports = {
     name: 'wordrelpay-setting',
@@ -16,13 +19,35 @@ module.exports = {
             loader: 'babel-loader',
             options: {
                 presets: [
-                    '@babel/preset-env', '@babel/preset-react',
+                    ['@babel/preset-env',{
+                        targets: {
+                          browsers: ['> 1% in KR'] // browserslist
+                        },
+                        debug: true,
+                    }],
+                    '@babel/preset-react',
                 ],
-            }
+                plugins: [
+                    '@babel/plugin-proposal-class-properties',
+                    'react-refresh/babel'
+                ]
+            },
+            exclude: path.join(__dirname, 'node_modules'),
         }]
     },
+    plugins: [
+      new webpack.LoaderOptionsPlugin({debug: true}),
+      new RefreshWebpackPlugin()
+
+    ],
     output: {
         path: path.join(__dirname, 'dist'),
-        filename: 'app.js'
+        filename: '[name].js',
+        publicPath: '/dist',
     }, // 출력
+    devServer: {
+        devMiddleware: { publicPath: '/dist' },
+        static: { directory: path.resolve(__dirname) },
+        hot: true
+    }
 }
