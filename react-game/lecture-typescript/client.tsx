94a3dd6057cf00afe636d5ab3f619c29e99cdb3e
SongMinJun <dkaskgkdua@gmail.com>
2022-02-01 22:45:04 +0900

    1. 지뢰찾기 타입스크립트 적용

--- react-game/lecture-typescript/client.tsx
+++ react-game/lecture-typescript/client.tsx
@@ -4,5 +4,6 @@ import ReactDOM from 'react-dom';
 // import GuGuDan from "./GuGuDan";
 // import ResponseCheck from "./ResponseCheck";
 // import HOT from "./RSP";
-import HOT from "./TicTacToe";
+// import HOT from "./TicTacToe";
+import HOT from "./MineSearch";
 ReactDOM.render(<HOT/>, document.querySelector("#root"));
