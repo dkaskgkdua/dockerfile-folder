fdcd06f04629060b4cd6a97ae5b042d509ff2ef4
SongMinJun <dkaskgkdua@gmail.com>
2022-02-02 01:28:12 +0900

    1. lazy-loading 적용 및 테스트

--- vue-lecture/vue-lecture/src/router/index.js
+++ vue-lecture/vue-lecture/src/router/index.js
@@ -13,7 +13,14 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
+    // 방문되는 순간 가져옴
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/contact',
+    name: 'Contact',
+    // prefetch 적용 -> 첫 로딩 시 캐시에 올라오게 됨.
+    component: () => import(/* webpackChunkName: "contact", webpackPrefetch:true */ '../views/Contact.vue')
   }
 ]
 
