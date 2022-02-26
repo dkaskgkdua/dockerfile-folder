fd48709133f78c6208d4b130db86919aa3d103cf
SongMinJun <dkaskgkdua@gmail.com>
2022-02-26 21:35:43 +0900

    reaction, action 적용 테스트

--- react-game/redux-vs-mobx/mobx/index.js
+++ react-game/redux-vs-mobx/mobx/index.js
@@ -1,15 +1,29 @@
-const { observable, autorun, runInAction } =  require('mobx');
+const { observable, autorun, runInAction, reaction, action } =  require('mobx');
 
 const state = observable({
     compA: 'a',
     compB: 12,
     compC: null,
 });
-// 바뀐걸 감지 -> 콜백
+// 모든 상태 바뀐걸 감지 -> 콜백
 autorun(() => {
     console.log('changed' + state.compA);
 })
-// runInAction 단위로 커밋
+
+// 첫번째 인수(함수)에서 리턴하는 값이 바뀌면 실행
+reaction(() => {
+    return state.compB;
+}, () => {
+    console.log("리엑션 : " +state.compB);
+})
+//필요한 타이밍에 실행하고 싶을 경우
+const change = action(() => {
+    state.compA = 'bb';
+    state.compB = 55;
+    state.compC = 'bb';
+})
+
+// runInAction 단위로 커밋, 바로실행
 runInAction(() => {
     state.compA = 'b';
     state.compB = 13;
@@ -17,3 +31,6 @@ runInAction(() => {
 runInAction(() => {
     state.compA = 'c';
 });
+
+// 지금 타이밍에 액셜 싱행
+change();
