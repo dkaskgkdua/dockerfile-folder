5d35e10c163a2c2ea8249902ec965d5cafc9c7eb
SongMinJun <dkaskgkdua@gmail.com>
2022-03-10 00:27:02 +0900

    1. link 설정과 eslint 세팅

--- prepare/front/pages/profile.js
+++ prepare/front/pages/profile.js
@@ -1,7 +1,10 @@
+import AppLayout from "../components/AppLayout";
 
 const Profile = () => {
     return (
+        <AppLayout>
         <div>프로필 페이지</div>
+        </AppLayout>
     )
 }
 export default Profile;
--- prepare/front/pages/signup.js
+++ prepare/front/pages/signup.js
@@ -1,7 +1,10 @@
+import AppLayout from "../components/AppLayout";
 
 const Signup = () => {
     return (
+        <AppLayout>
         <div>회원가입</div>
+        </AppLayout>
     )
 }
 export default Signup;
