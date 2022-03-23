ac5eac3f39c4aa65286b9dbef2bdc9c9e6b54df8
SongMinJun <dkaskgkdua@gmail.com>
2022-03-24 00:42:47 +0900

    1. 커스텀 useState 적용 2. 회원가입 폼 구현

--- prepare/front/components/LoginForm.js
+++ prepare/front/components/LoginForm.js
@@ -2,6 +2,8 @@ import React, {useCallback, useState, useMemo} from "react";
 import {Button, Form, Input} from "antd";
 import Link from "next/link";
 import styled from "styled-components"
+import PropTypes from "prop-types";
+import useInput from "../hooks/useInput";
 
 const ButtonWrapper = styled.div`
     margin-top: 10px;
@@ -11,15 +13,8 @@ const FormWrapper = styled(Form)`
 `;
 
 const LoginForm = ({ setIsLoggedIn }) => {
-    const [id, setId] = useState("");
-    const [password, setPassword] = useState("");
-
-    const onChangeId = useCallback((e) => {
-        setId(e.target.value);
-    }, []);
-    const onChangePassword = useCallback((e) => {
-        setPassword(e.target.value);
-    }, []);
+    const [id, onChangeId] = useInput("");
+    const [password, onChangePassword] = useInput("");
 
     // 렌더링 최적화 (스타일드 컴포넌트 적용 안할 시에)
     const style = useMemo(() => ({ marginTop: 10}), []);
@@ -54,5 +49,7 @@ const LoginForm = ({ setIsLoggedIn }) => {
         </FormWrapper>
     );
 }
-
+LoginForm.propTypes = {
+    setIsLoggedIn: PropTypes.func.isRequired
+}
 export default LoginForm;
