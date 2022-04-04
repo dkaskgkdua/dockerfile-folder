6de6ba83161c321b3ae389709633a6bf0ac67a60
SongMinJun <dkaskgkdua@gmail.com>
2022-04-05 00:43:53 +0900

    1. 포스트 이미지 슬라이드 팝업 추가

--- prepare/front/components/PostImages.js
+++ prepare/front/components/PostImages.js
@@ -1,6 +1,7 @@
 import React, {useCallback, useState} from "react";
 import PropTypes from "prop-types";
 import {PlusOutlined} from "@ant-design/icons";
+import ImagesZoom from "./ImagesZoom";
 
 const PostImages = ({ images }) => {
     const [showImagesZoom, setShowImagesZoom] = useState(false);
@@ -8,10 +9,16 @@ const PostImages = ({ images }) => {
     const onZoom = useCallback(() => {
         setShowImagesZoom(true);
     }, []);
+
+    const onClose = useCallback(() => {
+        setShowImagesZoom(false);
+    }, [])
+
     if (images.length === 1) {
         return (
             <>
                 <img role="presentation" src={images[0].src} alt={images[0].src} onClick={onZoom} />
+                {showImagesZoom && <ImagesZoom images={images} onClose={onClose}/>}
             </>
         )
     }
@@ -20,6 +27,7 @@ const PostImages = ({ images }) => {
             <>
                 <img role="presentation" style={{ width: "50%", display: "inline-block"}} src={images[0].src} alt={images[0].src} onClick={onZoom} />
                 <img role="presentation" style={{ width: "50%", display: "inline-block"}} src={images[0].src} alt={images[0].src} onClick={onZoom} />
+                {showImagesZoom && <ImagesZoom images={images} onClose={onClose}/>}
             </>
         )
     }
@@ -39,6 +47,7 @@ const PostImages = ({ images }) => {
 
                 </div>
             </div>
+            {showImagesZoom && <ImagesZoom images={images} onClose={onClose}/>}
 
         </>
     )
