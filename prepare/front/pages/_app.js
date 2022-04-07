f67bdbe04a33b72dc0710b7650d788299ab17dc0
SongMinJun <dkaskgkdua@gmail.com>
2022-04-07 21:57:16 +0900

    1. 리덕스 사가 추가

--- prepare/front/pages/_app.js
+++ prepare/front/pages/_app.js
@@ -2,6 +2,7 @@ import "antd/dist/antd.css";
 import PropTypes from "prop-types";
 import Head from "next/head";
 import wrapper from "../store/configureStore";
+import withReduxSaga from "next-redux-saga";
 
 // 전체 페이지에 공통적인 적용함
 const NodeBird = ({ Component }) => {
@@ -19,4 +20,4 @@ const NodeBird = ({ Component }) => {
 NodeBird.propTypes = {
     Component: PropTypes.elementType.isRequired,
 }
-export default wrapper.withRedux(NodeBird);
+export default wrapper.withRedux(withReduxSaga(NodeBird));
