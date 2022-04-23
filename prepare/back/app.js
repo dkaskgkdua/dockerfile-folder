208a0cd06a6644b04b8c23d112d18a7651defecf
SongMinJun <dkaskgkdua@gmail.com>
2022-04-23 23:51:09 +0900

    1. 시퀼라이즈 sync 및 nodemon 적용 테스트

--- prepare/back/app.js
+++ prepare/back/app.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const app = express();
+const db = require("./models");
 const postRouter = require("./routes/post")
+db.sequelize.sync()
+    .then(() => {
+        console.log("db 연결 성공")
+    })
+    .catch(console.error);
 
 app.get("/", (req, res) => {
    res.send("hello express");
@@ -15,3 +21,5 @@ app.use("/post", postRouter);
 app.listen(3065, () => {
     console.log("서버 실행 중 ")
 })
+
+// npx sequelize db:create (ORM db 생성)
